68c4fdd6afe6f1de4ea828c873c9b1312c6950f2
Charly <huillcas.juan3@gmail.com>
2024-11-11 10:41:06 -0500

    improve page

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ input_dir = "data/input/"
 output_dir = "data/output/"
 executable = "bin/cimg_lab"  # Path to the C++ executable
 
+# Set wide of window
+# st.set_page_config(layout="wide")
+
 # Application title
 st.title("Image Processing Application")
 
 # Option to upload an image or use the default image
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 use_default_image = st.checkbox("Use default image (eagle.jpg)")
 
 # Define paths for input and output images
@@ -25,9 +28,21 @@ if uploaded_file is not None:
         f.write(uploaded_file.getbuffer())
 elif use_default_image:
     input_path = os.path.join(input_dir, "eagle.jpg")
+    original_image = Image.open(input_path)
 
-# Add a selectbox for choosing the operation
-operation = st.selectbox("Choose an operation", ["Gradient", "Blur"])
+# Add a select box for choosing the operation
+operation = st.selectbox("Choose an operation",
+                         ["Gradient", "Blur", "math_transformations",
+                          "fill", "contrast"])
+
+# Show original image
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+else:
+    try:
+        st.image(original_image, caption="Default Image", use_container_width=True)
+    except:
+        st.warning("Please upload an image or select the default image option.")
 
 # Map operation to command-line argument
 operation_arg = operation.lower()  # Matches the names used in main.cpp
@@ -40,7 +55,7 @@ if st.button("Calculate"):
 
         if result.returncode == 0:
             result_image = Image.open(output_path)
-            st.image(result_image, caption=f"{operation} Image")
+            st.image(result_image, caption=f"{operation} Image", use_container_width=True)
             st.success(f"{operation} calculation complete.")
         else:
             st.error("There was an error processing the image.")
--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,37 @@ CImg<unsigned char> apply_blur(const CImg<unsigned char>& image) {
     return image.get_blur(2.0);
 }
 
+// Function to trasform img
+CImg<unsigned char> math_transformations(const CImg<unsigned char>& image) {
+    // Apply a series of transformations
+    CImgList<> mathOps(image, image.get_exp()/50, 10*image.get_sqrt(), (1+image.get_abs()).log(), image.get_pow(3));
+    // Combine all transformations into a single image (in a row)
+    CImg<unsigned char> combined_image = mathOps.get_append('x'); // Append images horizontally
+    return  combined_image;
+}
+
+// Function to fill image
+CImg<unsigned char> apply_fill(const CImg<unsigned char>& image) {
+    CImg<unsigned char> filled_image = image.get_fill("(x*y)%255",true);
+    return filled_image;
+}
+
+// enhance contrast
+CImg<unsigned char> adjust_contrast(const CImg<unsigned char>& image) {
+    CImg<unsigned char> imgOut;
+
+    if (image.spectrum() == 1) {
+        imgOut = image.get_equalize(256);
+    } else if (image.spectrum() == 3) {
+        CImg<unsigned char> R = image.get_channel(0).get_equalize(256);
+        CImg<unsigned char> G = image.get_channel(1).get_equalize(256);
+        CImg<unsigned char> B = image.get_channel(2).get_equalize(256);
+        CImgList<unsigned char> channels(R, G, B);
+        imgOut = channels.get_append('c');
+    }
+    return imgOut;
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 4) {
         std::cerr << "Usage: " << argv[0] << " <operation> <input_image_path> <output_image_path>" << std::endl;
@@ -33,13 +64,18 @@ int main(int argc, char* argv[]) {
     const char* input_path = argv[2];
     const char* output_path = argv[3];
     CImg<unsigned char> image(input_path);
-
     CImg<unsigned char> result_image;
 
     if (operation == "gradient") {
         result_image = compute_gradient(image);
     } else if (operation == "blur") {
         result_image = apply_blur(image);
+    } else if (operation == "math_transformations") {
+        result_image = math_transformations(image);
+    } else if (operation == "fill") {
+        result_image = apply_fill(image);
+    } else if (operation == "contrast") {
+        result_image = adjust_contrast(image);
     } else {
         std::cerr << "Unknown operation: " << operation << std::endl;
         return 1;
